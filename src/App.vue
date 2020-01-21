616dc3b43b9da4a31300ff042e0c16d00121bf5d
fishu <pkaczoruk@wp.pl>
2020-01-21 14:05:31 +0100

    add and style navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
   <div>
+    <Navbar />
     <Home />
   </div>
 </template>
 
 <script>
 import Home from './views/Home.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app',
   components: {
-    Home
+    Home,
+    Navbar
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+  * {
+    @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
 
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <h1>ToDo</h1>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style lang="scss" scoped>
+  nav {
+    height: 62px;
+    width: 100vw;
+    background: #363434;
+    box-shadow: 0 2px 4px #3634347e;
+  }
+
+  h1 {
+    color: white;
+    text-align: center;
+    line-height: 62px;
+  }
+</style>
